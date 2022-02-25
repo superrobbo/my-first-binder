4a16f200002d61d9515436c5782f76276c34011e
robertofedericosoft <59876233+robertofedericosoft@users.noreply.github.com>
2022-02-25 12:17:21 +0100

    Add files via upload
    sample query

---
+++ querysample.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
