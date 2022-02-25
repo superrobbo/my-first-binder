7ba18843618291d1e5a765d99abb060c2c41f298
robertofedericosoft <59876233+robertofedericosoft@users.noreply.github.com>
2022-02-25 17:11:51 +0100

    Update getapijson.sh
    run script from bash

--- getapijson.sh
+++ getapijson.sh
@@ -3,3 +3,14 @@ echo "loading openapi file"
 
 curl -X GET "http://127.0.0.1:8000/openapi.json" >> output.json
 
+curl -X PUT "http://127.0.0.1:8000/itemscomplex/1" -H 'Content-Type: application/json' -d '{"item": {
+        "name": "Foo",
+        "description": "The pretender",
+        "price": 42.0,
+        "tax": 3.2
+    },
+    "user": {
+        "username": "dave",
+        "full_name": "Dave Grohl"
+    }
+}'
