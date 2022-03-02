aee9da20107d256b07635e96c1334789f25ab483
robertofedericosoft <59876233+robertofedericosoft@users.noreply.github.com>
2022-03-02 10:19:15 +0100

    Update getapijson.sh
    run python curl

--- getapijson.sh
+++ getapijson.sh
@@ -3,14 +3,33 @@ echo "loading openapi file"
 
 curl -X GET "http://127.0.0.1:8000/openapi.json" >> output.json
 
-curl -X PUT "http://127.0.0.1:8000/itemscomplex/1" -H 'Content-Type: application/json' -d '{"item": {
-        "name": "Foo",
-        "description": "The pretender",
-        "price": 42.0,
-        "tax": 3.2
-    },
-    "user": {
-        "username": "dave",
-        "full_name": "Dave Grohl"
-    }
+<<comm
+curl -X POST "http://127.0.0.1:8000/" -H  "accept: application/json" -H  "Content-Type: application/json" -d "{\"test_key\":\"test_val\"}"
+comm
+
+
+echo 
+echo "put"
+
+curl -X PUT "http://127.0.0.1:8000/items/100" -H "Content-Type: application/json" -d '
+{
+    "name": "Foo",
+    "description": "The pretender",
+    "price": 42.0,
+    "tax": 3.2,
+    "tags": [
+        "rock",
+        "metal",
+        "bar"
+    ],
+    "images": [
+        {
+            "url": "http://example.com/baz.jpg",
+            "name": "The Foo live"
+        },
+        {
+            "url": "http://example.com/dave.jpg",
+            "name": "The Baz"
+        }
+    ]
 }'
