b34720bd330954e25f636dc5e0b4cbb1295989b7
robertofedericosoft <59876233+robertofedericosoft@users.noreply.github.com>
2022-07-14 15:23:34 +0200

    Update convertcsvtojson.py
    new version read file json

--- convertcsvtojson.py
+++ convertcsvtojson.py
@@ -1,11 +1,27 @@
 import csv
 import json
+from collections import OrderedDict 
 
 csvfile = open('my-data/import.csv', 'r')
-jsonfile = open('my-data/file.json', 'w')
+### jsonfile = open('my-data/file.json', 'w') 
 
-fieldnames = ("ID","Artist","Song", "Artist")
-reader = csv.DictReader( csvfile, fieldnames)
-for row in reader:
-    json.dump(row, jsonfile)
-    jsonfile.write('\n')
+fieldnames = ("ID","Artist","Song", "Album")
+entries = []
+
+with open('my-data/import.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames,delimiter=";")   
+    for row in reader:
+        entry = OrderedDict()
+        print(row)
+        for field in fieldnames:
+            entry[field] = row[field]
+        entries.append(entry)
+        
+output = {
+    "Music": entries
+}
+
+with open('my-data/file.json', 'w') as jsonfile:
+    json.dump(output, jsonfile)
+    jsonfile.write('\n')        
+
